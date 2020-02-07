7f917639ce147a91dc7af42c93b11e44f5ef07ef
Gokul Shanth <gokul.shanth@gmail.com>
2020-02-07 11:55:56 -0500

    object oriented stuff

---
+++ OOP_gokul/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Car
+{
+	public string colour = "black";
+	int maxSpeed = 200;
+	public int year;
+	public string model;
+	string registration
+	{ get; set; }
+public void fullThrottle()   
+	{
+		Console.WriteLine("The car is going as fast as it can!");
+	}
+	public Car()
+	{
+		this.model = "Toyota";
+	}
+	public Car(string model)
+	{
+		this.model = model;
+	}
+	public Car(string model, string colour, int year)
+	{
+		this.model = model;
+		this.colour = colour;
+		this.year = year;
+	}
+
+}
---
+++ OOP_gokul/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP_gokul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            Console.WriteLine(car.colour);
+            car.fullThrottle();
+
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.colour = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.colour = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+
+            Car toyota = new Car();
+            Console.WriteLine(car.model);
+
+            Car AstonMartin = new Car("Aston Martin");
+            Console.WriteLine(AstonMartin.model);
+
+            Car Ford1 = new Car("Mustang", "Red", 1969);
+            Console.WriteLine(Ford1.colour + " " + Ford1.year + " " + Ford1.model);
+
+        }
+    }
+}
